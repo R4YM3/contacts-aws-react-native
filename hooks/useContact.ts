bcec8abff7a347bba3df2042308eca5822a60798
Raymond Schweers <raymond@hetwebbureau.nl>
2022-06-27 22:58:38 +0200

    fix(useContact) typing

--- hooks/useContact.ts
+++ hooks/useContact.ts
@@ -2,7 +2,6 @@ import { useQuery } from "react-query"
 import { fetchContact, IFetchContactQueryKey } from "../api/contacts";
 
 export const useContact = (userId: number) => {
-  if (!userId) return {};
   const queryKey: IFetchContactQueryKey = ['contact', { userId }];
   return useQuery(queryKey, fetchContact);
 }
